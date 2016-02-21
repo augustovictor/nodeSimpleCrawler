d2dd117dfb7ebb45d877ecdb2b1aa19bc4c53de1
Victor <victoraweb@gmail.com>
2016-02-20 23:19:55 -0300

    Sending post request with captcha

--- scrape.js
+++ scrape.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var requestify = require('requestify'); 
 
 var url = 'http://www.anp.gov.br/preco/prc/Resumo_Por_Estado_Index.asp';
 
@@ -10,14 +11,26 @@ request(url, function (error, response, html) {
 
     if (!error && response.statusCode === 200) {
 
-        var $ = cheerio.load(html),
-            captcha = '';
+        var $ = cheerio.load(html);
+        var captcha = '';
 
         $("#divQuadro label[id*='letra']").each(function () {
             captcha += $(this).text();
         });
-
+        
         console.log(captcha);
+//        console.log(html);
+        $('#txtValor').val(captcha);
+
+        requestify.post(url, {
+                txtValor: captcha
+            })
+            .then(function (response) {
+                // Get the response body
+                console.log(response);
+            });
+
+
         response.responseTime = new Date() - start;
         console.log(response.responseTime + ' ms');
 
